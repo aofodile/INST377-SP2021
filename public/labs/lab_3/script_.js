431edcbe4356aa37fffced116784804290c52829
aofodile <70602087+aofodile@users.noreply.github.com>
2021-02-10 15:27:10 -0500

    working

---
+++ public/labs/lab_3/script_.js
@@ -0,0 +1,78 @@
+/* Put your javascript in here */
+
+let imgs = ["onigiri_1.png","onigiri_2.png","onigiri_3.png","roll_1.png","roll_2.png","roll_3.png"];
+
+document.getElementById("mainView").style.color = "blue"
+
+rthrth jhtyjhty
+// imgs.forEach(myFunction);
+// document.getElementById("mainView").innerHTML = imgs[0];
+
+// function myFunction(value) {
+//   txt = txt + value + "<br>"; 
+// }
+
+// // document.getElementById("navRight").addEventListener("click", scrollRight);
+
+// // let mainImg = 0;
+// // let prevImg = imgs.length - 1;
+// // let nextImg = 1;
+
+// let mainView = document.getElementById("mainView");
+// mainView.style.backgroundImage = "url('onigiri_3.png')";
+
+
+// function loadGallery() {
+// 	let mainView = document.getElementById("mainView");
+// 	mainView.style.backgroundImage = "url(" + imgs[prevImg] + ")";
+
+//   	// let leftView = document.getElementById("leftView");
+//   	// leftView.style.background = "url(" + imgs[prevImg] + ")";
+  
+//   	// let rightView = document.getElementById("rightView");
+//   	// rightView.style.background = "url(" + imgs[nextImg] + ")";
+  
+//   	let linkTag = document.getElementById("linkTag")
+//   	linkTag.href = imgs[mainImg];
+
+// };
+// loadGallery()
+
+// function scrollRight() {
+// 	prevImg = mainImg;
+//   	mainImg = nextImg;
+//   	if (nextImg >= (imgs.length -1)) {
+//   		nextImg = 0;
+//   	} else {
+//   		nextImg++;
+//   	}; 
+//   	loadGallery();
+// };
+
+// // function scrollLeft() {
+// // 	nextImg = mainImg
+// //   	mainImg = prevImg;
+   
+// //   	if (prevImg === 0) {
+// //   		prevImg = imgs.length - 1;
+// //   	} else {
+// //   		prevImg--;
+// //   	};
+// // //   	loadGallery();
+// // };
+
+// document.getElementById("navRight").addEventListener("click", scrollRight);
+// // document.getElementById("navLeft").addEventListener("click", scrollLeft);
+// // document.getElementById("rightView").addEventListener("click", scrollRight);
+// // document.getElementById("leftView").addEventListener("click", scrollLeft);
+// // document.addEventListener('keyup',function(e){
+
+// //     if (e.keyCode === 37) {
+// //     scrollLeft();
+
+// //   	} else if(e.keyCode === 39) {
+// //   		scrollRight();
+// //   	}
+// // });
+// scrollRight()
+// loadGallery();
