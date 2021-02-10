04dd331ce5cdc4e836cee08b7de82b4bd76e7920
aofodile <70602087+aofodile@users.noreply.github.com>
2021-02-10 17:27:19 -0500

    changed js

--- public/labs/lab_3/app.js
+++ public/labs/lab_3/app.js
@@ -1,5 +1,14 @@
 /* Put your javascript in here */
 
-// let imgs = ["onigiri_1.png","onigiri_2.png","onigiri_3.png","roll_1.png","roll_2.png","roll_3.png"];
-document.getElementById("test").innerHTML = "Hello"
+let imgs = ["onigiri_1.png","onigiri_2.png","onigiri_3.png","roll_1.png","roll_2.png","roll_3.png"];
 
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM fully loaded and parsed");
+  });
+
+function test() {
+    alert('Click!');
+};
+
+const main = document.getElementById("main");
+main.addEventListener("mouseover", test());
